876784ddea7dbfff85238aa5fb5a893e54397479
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-12 22:08:37 +0000

    add cyberdream theme

---
+++ lua/custom/plugins/cyberdream.lua
@@ -0,0 +1,12 @@
+return {
+  'scottmckendry/cyberdream.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('cyberdream').setup {
+      variant = 'default',
+      transparent = true, -- Set to true to enable transparency
+    }
+    vim.cmd 'colorscheme cyberdream' -- Add this line here
+  end,
+}
