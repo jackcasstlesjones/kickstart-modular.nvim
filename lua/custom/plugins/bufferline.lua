5182474c05c77daf9520447b2005f54bcaf683ec
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-12 22:36:19 +0000

    inital bufferline setup

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,3 +1,29 @@
 return {
-    {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bufferline').setup {
+        options = {
+          numbers = 'none',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          separator_style = 'thin',
+          show_buffer_close_icons = true,
+          show_close_icon = true,
+          style_preset = {
+            require('bufferline').style_preset.no_italic,
+          },
+        },
+      }
+
+      -- Keymaps for switching buffers
+      vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev buffer' })
+      vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer' })
+      vim.keymap.set('n', '<leader>x', '<cmd>bdelete<cr>', { desc = 'Close current buffer' })
+    end,
+  },
 }
