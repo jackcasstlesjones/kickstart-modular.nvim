1b54ceeff7d5698ec00f599a188a416cba4d1a84
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-12 22:10:02 +0000

    disable snacks explorer, use snacks lazygit

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,32 +1,45 @@
 return {
-    {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = true },
-    dashboard = { enabled = false },
-    explorer = { enabled = true },
-      lazygit = {enabled = true},
-    indent = { enabled = true },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
-      zen = {enabled = true}
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      animate = { enabled = false },
+      bigfile = { enabled = true },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      lazygit = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = false },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      zen = { enabled = true },
+    },
+    keys = {
+      -- Top Pickers & Explorer
+      {
+        '<leader>e',
+        function()
+          Snacks.explorer()
+        end,
+        desc = 'File Explorer',
+      },
+      {
+        '<leader>gg',
+        function()
+          Snacks.lazygit()
+        end,
+        desc = 'LazyGit',
+      },
+    },
   },
-      keys = {
-    -- Top Pickers & Explorer
-    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "LazyGit" },
-}
-}
 }
