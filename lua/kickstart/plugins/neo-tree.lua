d64af667e259fe90c9a1f8101a585235be83c91a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-15 23:55:39 +0000

    add comment for project nvim integration with neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -69,6 +69,7 @@ return {
     sources = { 'filesystem', 'buffers', 'git_status' },
     open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
     filesystem = {
+      -- bind_to_cwd = true, -- enable this to switch neo tree to new project when using project.nvim
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
