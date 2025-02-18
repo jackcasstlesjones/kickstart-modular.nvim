cc0e02f63f36b3981a83c86aaadc72a051f20642
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-18 09:25:33 +0000

    kanagawa

--- lua/custom/plugins/nordfox.lua
+++ lua/custom/plugins/nordfox.lua
@@ -1,6 +1,6 @@
 return {
   'EdenEast/nightfox.nvim',
-  enabled = true,
+  enabled = false,
   priority = 1000, -- Ensures the colorscheme loads early
   config = function()
     require('nightfox').setup {
