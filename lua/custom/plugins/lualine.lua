bc19f4b01221b4dfcf97b68f58f9a6156269a593
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-19 20:28:20 +0000

    nord theme for lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -13,7 +13,8 @@ return {
       require('lualine').setup {
         options = {
           icons_enabled = true,
-          theme = 'powerline_dark',
+          theme = 'nord',
+          -- theme = 'powerline_dark',
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
           disabled_filetypes = {
