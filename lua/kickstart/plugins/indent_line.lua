904dc7b9dba0c03ed3a273929dd4abc5c1e62499
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-19 17:02:13 +0000

    remove underline from indent_line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -18,13 +18,13 @@ return {
       local hooks = require 'ibl.hooks'
 
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD', underline = true })
-        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2', underline = true })
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD', underline = false })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2', underline = false })
       end)
 
       vim.g.rainbow_delimiters = { highlight = highlight }
