964fd51381f9d3c5fc91a2ce5052e8c80512ba70
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-20 21:55:59 +0000

    add cutlass

---
+++ lua/custom/plugins/cutlass.lua
@@ -0,0 +1,17 @@
+return {
+  -- Lua
+  {
+    'gbprod/cutlass.nvim',
+    opts = {
+      cut_key = 'x',
+      override_del = nil,
+      exclude = {},
+      registers = {
+        select = '_',
+        delete = '_',
+        change = '_',
+      },
+    },
+  },
+}
+-- Lua
