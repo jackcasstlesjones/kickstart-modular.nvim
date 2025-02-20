927d87070067c005133bac82ea7f0b520a129aed
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-20 21:44:02 +0000

    disable markdown prettier formatting in conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,38 +1,39 @@
-return { "stevearc/conform.nvim",
+return {
+  'stevearc/conform.nvim',
   enabled = true,
-  event = { "BufReadPre", "BufNewFile" },
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require("conform")
+    local conform = require 'conform'
 
-    conform.setup({
+    conform.setup {
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        -- markdown = { "prettier" },
+        graphql = { 'prettier' },
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
       },
       format_on_save = {
         lsp_fallback = true,
         async = false,
         timeout_ms = 500,
       },
-    })
+    }
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
+    vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
+      conform.format {
         lsp_fallback = true,
         async = false,
         timeout_ms = 500,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+      }
+    end, { desc = 'Format file or range (in visual mode)' })
   end,
 }
