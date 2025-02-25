661949ec5d0d6978ab7fec61d2bd488bb3b8efcc
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-25 11:16:50 +0000

    custom renderer for markview

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -3,6 +3,21 @@ return {
   'OXY2DEV/markview.nvim',
   lazy = false,
   config = function()
+    local presets = require 'markview.presets'
+
+    require('markview').setup {
+      markdown = {
+        headings = presets.headings.marker,
+      },
+      renderers = {
+        -- Custom renderer for markdown list items (bullet points)
+        markdown_list_item = function(ns, buffer, item)
+          -- Empty renderer to effectively disable bullet points
+          -- By providing a custom renderer that does nothing,
+          -- we prevent the default rendering of bullet points
+        end,
+      },
+    }
     vim.cmd 'Markview Enable'
   end,
   keys = {
