a5f6731172ffb8caa23a6ff6b9be99754ccf028e
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-28 22:59:58 +0000

    remove tilda from empty lines

--- lua/options.lua
+++ lua/options.lua
@@ -71,4 +71,6 @@ vim.opt.confirm = true
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 
+-- Remove tilda from empty lines
+vim.opt.fillchars = { eob = ' ' }
 -- vim: ts=2 sts=2 sw=2 et
