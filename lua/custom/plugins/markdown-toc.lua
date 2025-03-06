bee97187b2a45fe7cf99a7755728372eda552021
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-03-06 21:41:01 +0000

    disable markdown toc

--- lua/custom/plugins/markdown-toc.lua
+++ lua/custom/plugins/markdown-toc.lua
@@ -1,6 +1,7 @@
 return {
   {
     'hedyhli/markdown-toc.nvim',
+    enabled = false,
     ft = 'markdown', -- Lazy load on markdown filetype
     cmd = { 'Mtoc' }, -- Or, lazy load on "Mtoc" command
     opts = {
